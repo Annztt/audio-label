6233387406c2fe12e9aae88582786fad41df02dd
Annztt <603971407@qq.com>
2017-06-23 14:24:02 +0800

    first commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "2.标记";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(24, 37);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(35, 16);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "dl";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(24, 59);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(35, 16);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "dx";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(24, 81);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(35, 16);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "dr";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(24, 103);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(35, 16);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "zy";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(24, 125);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(35, 16);
+            this.radioButton5.TabIndex = 9;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "jy";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(32, 37);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(35, 16);
+            this.radioButton6.TabIndex = 10;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "jj";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.CheckAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.radioButton7.Location = new System.Drawing.Point(32, 59);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(35, 16);
+            this.radioButton7.TabIndex = 11;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "xj";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(32, 84);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(35, 16);
+            this.radioButton8.TabIndex = 12;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "zx";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton5);
+            this.panel1.Location = new System.Drawing.Point(11, 86);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(106, 153);
+            this.panel1.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "说话人";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.radioButton8);
+            this.panel2.Controls.Add(this.radioButton6);
+            this.panel2.Controls.Add(this.radioButton7);
+            this.panel2.Location = new System.Drawing.Point(153, 86);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(103, 119);
+            this.panel2.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "情绪";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(22, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(1263, 105);
+            this.axWindowsMediaPlayer1.TabIndex = 17;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(175, 135);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "播放";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(33, 135);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(97, 23);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "打开wav文件";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(13, 0);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(681, 313);
+            this.textBox2.TabIndex = 22;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "1.设断点";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(11, 297);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 23);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "3.从断点播放";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(613, 342);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(81, 23);
+            this.button7.TabIndex = 25;
+            this.button7.Text = "导出txt文档";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(511, 342);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 27;
+            this.button8.Text = "撤回一行";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(181, 250);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 28;
+            this.button9.Text = "撤回标记";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Controls.Add(this.button10);
+            this.panel3.Controls.Add(this.button9);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.panel2);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Location = new System.Drawing.Point(22, 175);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(332, 377);
+            this.panel3.TabIndex = 29;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.textBox2);
+            this.panel4.Controls.Add(this.button7);
+            this.panel4.Controls.Add(this.button8);
+            this.panel4.Location = new System.Drawing.Point(578, 135);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(707, 393);
+            this.panel4.TabIndex = 30;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(153, 10);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 29;
+            this.button10.Text = "快进";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "快退";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1297, 564);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        //static List<string> list = new List<string>();//定义list变量，存放获取到的路径
+        List<RadioButton> labelBoxs = new List<RadioButton>();
+        OpenFileDialog openFile = new OpenFileDialog();
+        Stack<Double> stopPoint = new Stack<Double>();
+        Double myDtime;
+        int linesNum = -1;
+        //private int cur = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //添加8个radioButton
+            labelBoxs.Add(this.radioButton1);
+            labelBoxs.Add(this.radioButton2);
+            labelBoxs.Add(this.radioButton3);
+            labelBoxs.Add(this.radioButton4);
+            labelBoxs.Add(this.radioButton5);
+            labelBoxs.Add(this.radioButton6);
+            labelBoxs.Add(this.radioButton7);
+            labelBoxs.Add(this.radioButton8);
+
+            //DirectoryInfo dir = new DirectoryInfo("G:/后摄像头可爱的西沙群岛  下乡明德小学赵丹羽公开课");
+            //FileInfo[] fil = dir.GetFiles();
+            //foreach (FileInfo f in fil)
+            //{
+            //    list.Add(f.FullName);//添加文件的路径到列表
+            //}   
+        }
+        
+        private void button6_Click(object sender, EventArgs e)
+        {
+           
+            //打开播放文件
+            openFile.ShowDialog();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //播放
+            axWindowsMediaPlayer1.URL = openFile.FileName;
+            String fullName = openFile.FileName;
+            stopPoint.Push(0.0000000);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //快进
+            if (axWindowsMediaPlayer1.Ctlcontrols.currentPosition < axWindowsMediaPlayer1.currentMedia.duration - 5)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.currentPosition += 5;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //快退
+            if (axWindowsMediaPlayer1.Ctlcontrols.currentPosition > 3 && axWindowsMediaPlayer1.Ctlcontrols.currentPosition > stopPoint.Peek() + 3)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.currentPosition -= 3;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //1.设断点
+            this.axWindowsMediaPlayer1.Ctlcontrols.pause();            
+            myDtime = this.axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+            Double beforeStop = stopPoint.Peek();
+            linesNum++;
+            textBox2.AppendText((linesNum+1) +" "+ beforeStop + " " + myDtime + " ");
+            stopPoint.Push(myDtime);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //2.标记
+            //String filename = list[this.cur];
+            //filename = filename.Substring(0, filename.Length - 4);
+            String myLabel = "";
+
+            foreach (RadioButton cb in this.labelBoxs)
+            {
+                if (cb.Checked)
+                {
+                    myLabel = myLabel + cb.Text + "-";
+                }
+            }
+            myLabel = myLabel.Substring(0, myLabel.Length - 1);
+
+            //clsPlaySound.ChangeFileName(list[this.cur], filename + "-" + myLabel + ".wav");
+            textBox2.AppendText("-" + myLabel + " \r\n");
+            //this.cur++;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //3.从断点播放
+            this.axWindowsMediaPlayer1.Ctlcontrols.currentPosition = myDtime;
+            this.axWindowsMediaPlayer1.Ctlcontrols.play();
+            
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //撤回一行
+            stopPoint.Pop();
+            int start = textBox2.GetFirstCharIndexFromLine(linesNum);//最后一行第一个字符的索引
+            int end = textBox2.Text.Length;//最后一行最后一个字符的索引
+            textBox2.Select(start, end);//选中一行
+            textBox2.SelectedText = "";//设置一行的内容为空
+            linesNum--;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //撤回标记 
+            int start = textBox2.Text.Length - 9;
+            int end = textBox2.Text.Length;
+            textBox2.Select(start, end);//选中
+            textBox2.SelectedText = "";//设置的内容为空
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //导出txt文件
+            // "保存为"对话框
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "文本文件|*.txt";
+            // 显示对话框
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                // 文件名
+                string fileName = dialog.FileName;
+                // 创建文件，准备写入
+                FileStream fs = File.Open(fileName,
+                        FileMode.Create,
+                        FileAccess.Write);
+                StreamWriter wr = new StreamWriter(fs);
+
+                // 逐行将textBox1的内容写入到文件中
+                foreach (string line in textBox2.Lines)
+                {
+                    wr.WriteLine(line);
+                }
+
+                // 关闭文件
+                wr.Flush();
+                wr.Close();
+                fs.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+
+    //private void button1_Click(object sender, EventArgs e)
+    //{
+    //    //连续播放时
+    //    if (this.cur < list.Count())
+    //    {
+    //        // play list[cur]
+    //        String fname = list[cur];
+    //        fname = fname.Substring(fname.Length - 10);
+    //        textBox1.Text = fname;
+    //        clsPlaySound.PlaySoundFile(list[cur], true);
+    //    }
+    //}
+
+    //public class clsPlaySound
+    //{
+    //    protected const int SND_SYNC = 0x0;
+    //    protected const int SND_ASYNC = 0x1;
+    //    protected const int SND_NODEFAULT = 0x2;
+    //    protected const int SND_MEMORY = 0x4;
+    //    protected const int SND_LOOP = 0x8;
+    //    protected const int SND_NOSTOP = 0x10;
+    //    protected const int SND_NOWAIT = 0x2000;
+    //    protected const int SND_ALIAS = 0x10000;
+    //    protected const int SND_ALIAS_ID = 0x110000;
+    //    protected const int SND_FILENAME = 0x20000;
+    //    protected const int SND_RESOURCE = 0x40004;
+    //    protected const int SND_PURGE = 0x40;
+    //    protected const int SND_APPLICATION = 0x80;
+
+    //    [DllImport("Winmm.dll", CharSet = CharSet.Auto)]
+    //    protected extern static bool PlaySound(string strFile, IntPtr hMod, int flag);
+
+    //    //播放声音函数
+    //    //strSoundFile   --- 声音文件
+    //    //bSynch         --- 是否同步，如果为True，则播放声音完毕再执行后面的操作，为False，则播放声音的同时继续执行后面的操作
+    //    public static bool PlaySoundFile(string strSoundFile, bool bSynch)
+    //    {
+    //        if (!System.IO.File.Exists(strSoundFile))
+    //            return false;
+    //        int Flags;
+    //        if (bSynch)
+    //            Flags = SND_FILENAME | SND_SYNC;
+    //        else
+    //            Flags = SND_FILENAME | SND_ASYNC;
+
+    //        return PlaySound(strSoundFile, IntPtr.Zero, Flags);
+    //    }
+
+    //    public static bool ChangeFileName(string srcFileName, string destFileName)
+    //    {
+    //        if (System.IO.File.Exists(srcFileName))
+    //        {
+    //            System.IO.File.Move(srcFileName, destFileName);
+    //            return true;
+    //        }
+    //        else
+    //            return false;
+    //    }
+
+    //}
+
+}
